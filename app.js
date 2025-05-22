0b0665ecf9b45f28873324bcdac9003dfb134fc2
Sachin <sachin@Sachins-MacBook-Air.local>
2025-05-22 23:08:59 +0530

    Responding to URL Parameters

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const tour = tours?.find((el) => {
+    return el.id == id;
+  });
+  if (!tour) {
+    return res.status(404).json({ status: "failed", message: "Invalid Id" });
+  }
+  res.status(200).json({ status: "success", data: { tour: tour } });
+});
 app.post("/api/v1/tours", (req, res) => {
   const newId = tours[tours.length - 1]?.id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
