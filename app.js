2afdb55ad72846b3a5774ddee9c28d078e686b89
Sachin <sachin@Sachins-MacBook-Air.local>
2025-05-23 01:05:40 +0530

    delete request

--- app.js
+++ app.js
@@ -49,6 +49,9 @@ app.patch("/api/v1/tours/:id", (req, res) => {
     .json({ status: "success", data: { tour: "<Updated Tour Here...>" } });
 });
 
+app.delete("/api/v1/tours/:id", (req, res) => {
+  res.status(204).json({ status: "success", data: null });
+});
 const PORT = 3000;
 
 app.listen(PORT, () => {
