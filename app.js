30587db35ac776ae259e7ae42798465866990505
Sachin <sachin@Sachins-MacBook-Air.local>
2025-05-23 01:33:18 +0530

    creating our own middleware

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const fs = require("fs");
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log("Hello from the middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
+    requestedAt: req.requestTime,
     results: tours?.length,
     data: { tours: tours },
   });
