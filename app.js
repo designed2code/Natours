315292b1536cf4538ab9d31af05ca123a4163eee
Sachin <sachin@Sachins-MacBook-Air.local>
2025-05-23 02:39:02 +0530

    create routes folder for tourRoutes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const fs = require("fs");
 const morgan = require("morgan");
+const tourRouter = require("./routes/tourRoutes");
 const app = express();
 
 app.use(morgan("dev"));
@@ -14,56 +14,6 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    requestedAt: req.requestTime,
-    results: tours?.length,
-    data: { tours: tours },
-  });
-};
-const getTour = (req, res) => {
-  const id = parseInt(req.params.id);
-
-  const tour = tours?.find((el) => {
-    return el.id == id;
-  });
-  if (!tour) {
-    return res.status(404).json({ status: "failed", message: "Invalid Id" });
-  }
-  res.status(200).json({ status: "success", data: { tour: tour } });
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1]?.id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({ status: "success", data: { tour: newTour } });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  const id = req.params.id;
-  const tour = tours.find((ele) => ele.id == id);
-  if (!tour) {
-    return res.status(404).json({ status: "failed", message: "Invalid Id" });
-  }
-  res
-    .status(200)
-    .json({ status: "success", data: { tour: "<Updated Tour Here...>" } });
-};
-
-const deleteTour = (req, res) => {
-  res.status(204).json({ status: "success", data: null });
-};
 
 const getAllUsers = (req, res) => {
   res
@@ -99,12 +49,9 @@ const deleteUser = (req, res) => {
 // app.patch("/api/v1/tours/:id", updateTour);
 // app.delete("/api/v1/tours/:id", deleteTour);
 
-const tourRouter = express.Router();
 const userRouter = express.Router();
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
-tourRouter.route("/").get(getAllTours).post(createTour);
-tourRouter.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
 userRouter.route("/").get(getAllUsers).post(createUser);
 userRouter.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
